e5b9464843fe7d03cabe5c66ca09e60f6bb35454
mala-zi <sandra.saki2002@gmail.com>
2025-01-05 11:24:39 +0100

    promena naziva migracije
    promena naziva migracije za omiljene destinacije zbog baze

--- itehLaravel/database/migrations/2025_01_05_101904_create_favorites_destination_table.php
+++ itehLaravel/database/migrations/2025_01_05_101904_create_favorites_destination_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('favorites', function (Blueprint $table) {
+        Schema::create('favorites_destination', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('destination_id')->constrained()->onDelete('cascade'); 
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('favorites');
+        Schema::dropIfExists('favorites_destination');
     }
 };
